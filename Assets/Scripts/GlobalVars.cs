c04fbb71d47051f45b0206f24ea493fbcc7df668
FilipVidovic <filipvidovic@gmx.at>
2015-11-17 12:38:04 +0100

    Globalvars
    Versuch, Globale Variablen reinzubekommen

---
+++ Assets/Scripts/GlobalVars.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVars : MonoBehaviour {
+
+	public static float maxtime = 99;
+	public static float wettime = 33;
+
+	public float getMaxTime()
+	{
+		return maxtime;
+	}
+	
+	public float getWetTime()
+	{
+		return wettime;
+	}
+}
--- Assets/Scripts/PlayerTimer.cs
+++ Assets/Scripts/PlayerTimer.cs
@@ -6,27 +6,30 @@ public class PlayerTimer : MonoBehaviour {
 
 	public RawImage timebar;
 	public float endTime;
+	//public GlobalVars globalvars;
+	public float maxtime = 99;
+	public float wettime = 33;
 
 	// Use this for initialization
 	void Start () {
-		endTime = Time.time + 60;
+		endTime = Time.time + maxtime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if ((endTime - Time.time) > 0) {
-			timebar.transform.localScale = new Vector3 ((endTime - Time.time) / 60f, 1, 1);
+			timebar.transform.localScale = new Vector3 ((endTime - Time.time) / maxtime, 1, 1);
 		} else
-			endTime += 60;
+			endTime += maxtime;
 		//thistime.transform.localScale = new Vector3(0, 1, 1);
 	}
 
 	public void wetLawyer()
 	{
-		endTime += 15;
+		endTime += wettime;
 
-		if (endTime - Time.time > 60) {
-			endTime = Time.time + 60;
+		if (endTime - Time.time > maxtime) {
+			endTime = Time.time + maxtime;
 		}
 	}
 }
